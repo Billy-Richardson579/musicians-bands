476d1c4e31cebb6688d3d112ad45d92e59194ed9
Billy Richardson <billy7373731@outlook.com>
2023-05-03 12:30:14 +0100

    db set up

--- db.js
+++ db.js
@@ -3,6 +3,13 @@ const { Sequelize, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
 
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname,"db.sqlite")
+});
+
+
+sequelize.sync();
 module.exports = {
     sequelize,
     Sequelize
